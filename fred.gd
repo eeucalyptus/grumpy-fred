6d29cd86e8b20e47e14045569e348d7d2a67e519
eeucalyptus <dev@eeucalyptus.net>
2017-11-17 22:42:36 +0100

    - Cleaned up debug fred animation - Added level

--- fred.gd
+++ fred.gd
@@ -5,9 +5,17 @@ extends Container
 # var b = "textvar"
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
-
+	self.set_process(true)
+	
+func _process(delta):
+	if(Input.is_mouse_button_pressed(BUTTON_LEFT)):
+		var cur_pos = self.get_pos()
+		cur_pos.x += 100 * delta
+		
+		# wrap around screen
+		if(cur_pos.x > self.get_viewport_rect().size.width + self.get_item_rect().size.width/2):
+			cur_pos.x = -self.get_item_rect().size.width/2
+		self.set_pos(cur_pos)
+		
 func setMode():
 	pass
